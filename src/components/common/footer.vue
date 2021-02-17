8e1cab0289d9a5fd5b3a90fe2ec758875fdf88a5
dansiapa <48487205+dansiapa@users.noreply.github.com>
2021-02-18 01:39:43 +0800

    Update footer.vue

--- src/components/common/footer.vue
+++ src/components/common/footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="relative bg-white px-3 py-4 text-center">
     <p class="text-blue-500">Get all pokemon data at one place. Powered by pokeAPI</p>
-    <p class="text-gray-700 my-3">copyright@vuedex all rights reserved 2020</p>
+    <p class="text-gray-700 my-3">copyright@ RDPF all rights reserved 2020</p>
   </footer> 
 </template>
 
