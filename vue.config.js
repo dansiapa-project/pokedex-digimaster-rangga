838aec0ff1c6340aee6987256ea653d43c6424df
dansiapa <48487205+dansiapa@users.noreply.github.com>
2021-02-18 01:43:18 +0800

    .

--- vue.config.js
+++ vue.config.js
@@ -3,21 +3,20 @@ module.exports = {
     crossorigin: 'anonymous',
     integrity: true,
     pages: {
-      index: {
-        entry: 'src/main.js',
-        title: 'Vuedex',
-      },
+        index: {
+            entry: 'src/main.js',
+            title: 'Pokedex Digimaster',
+        },
     },
     devServer: {
-      disableHostCheck: true,
-      useLocalIp: false,
-      proxy: {
-        '': {
-          // Configure with your DMS domain
-          target: 'https://pokeapi.co',
-          changeOrigin: true,
+        disableHostCheck: true,
+        useLocalIp: false,
+        proxy: {
+            '': {
+                // Configure with your DMS domain
+                target: 'https://pokeapi.co',
+                changeOrigin: true,
+            },
         },
-      },
     },
-  };
-  
+};
